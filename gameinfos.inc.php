45a701cc5e0eadbb26c0bd8344c67706be283182
mizutismask <severinek@gmail.com>
2024-03-28 15:13:33 +0100

    feat: makes the screen fit on mobile setting min game_interface_width

--- gameinfos.inc.php
+++ gameinfos.inc.php
@@ -93,11 +93,11 @@ $gameinfos = array(
 // Note: game interface = space on the left side, without the column on the right
 'game_interface_width' => array(
 
-    // Minimum width
-    //  default: 740
-    //  maximum possible value: 740 (ie: your game interface should fit with a 740px width (correspond to a 1024px screen)
-    //  minimum possible value: 320 (the lowest value you specify, the better the display is on mobile)
-    'min' => 740,
+        // Minimum width
+        //  default: 740
+        //  maximum possible value: 740 (ie: your game interface should fit with a 740px width (correspond to a 1024px screen)
+        //  minimum possible value: 320 (the lowest value you specify, the better the display is on mobile)
+        'min' => 975,
 
     // Maximum width
     //  default: null (ie: no limit, the game interface is as big as the player's screen allows it).
